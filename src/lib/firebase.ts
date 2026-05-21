d0a14a5383a262edd8e9d798f04838ce447271a7
himmetmuhammedk <himmetmuhammedk@gmail.com>
2026-05-21 10:31:22 +0300

    sd

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,6 +5,7 @@ import { getFirestore, doc, getDocFromServer } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDie4Pv8oIvTgQUlKJjEliMtrN-owTTxFk",
   authDomain: "dentlas.firebaseapp.com",
+  databaseURL: "https://dentlas-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "dentlas",
   storageBucket: "dentlas.firebasestorage.app",
   messagingSenderId: "900425983957",
